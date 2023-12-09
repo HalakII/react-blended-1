2974cc329e8c457bc9083ab820b26123305685dc
Andrii <132355436+anrisanches@users.noreply.github.com>
2023-12-09 12:51:07 +0200

    task2

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -1,9 +1,18 @@
+import { IconContext } from 'react-icons/lib/esm';
 import {
   StatisticBox,
   StatisticText,
   StatisticCounter,
 } from './StatisticItem.styled';
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+export const StatisticItem = ({ title, total, icon }) => {
+  return (
+    <StatisticBox>
+      <IconContext.Provider value={{ color: 'blue', size: '2em' }}>
+        {icon}
+      </IconContext.Provider>
+      <StatisticCounter>{total}</StatisticCounter>
+      <StatisticText>{title}</StatisticText>
+    </StatisticBox>
+  );
 };
